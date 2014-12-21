d4e2596e5957ae294f8828cf668e86c3269cfdee
axlecho <axlecho@gmail.com>
2014-12-21 14:40:01 +0800

    修复首页title消失问题

--- yiye/src/me/yiye/MainActivity.java
+++ yiye/src/me/yiye/MainActivity.java
@@ -142,7 +142,8 @@ public class MainActivity extends FragmentActivity {
 
         drawerListView.setItemChecked(1, true); // 设置第一项被按下
         drawderTitile = getResources().getString(R.string.drawer_open);
-
+        activityTitle = planetTitles[0];
+        getActionBar().setTitle(activityTitle);
     }
 
     protected void onStart() {
@@ -212,6 +213,7 @@ public class MainActivity extends FragmentActivity {
         MobclickAgent.onPageStart(TAG); //统计页面
         MobclickAgent.onResume(this);          //统计时长
     }
+
     public void onPause() {
         super.onPause();
         MobclickAgent.onPageEnd(TAG); // 保证 onPageEnd 在onPause 之前调用,因为 onPause 中会保存信息
